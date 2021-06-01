ad84b9d639b39e06add695277f4b1b365253df1e
Fillipek <marcin.filipek.321@gmail.com>
2021-06-01 22:14:30 +0200

    PROJ-48 Complete TestMeeting

--- Studentify.IntegrationTests/GetDto/MeetingGetDto.cs
+++ Studentify.IntegrationTests/GetDto/MeetingGetDto.cs
@@ -19,5 +19,65 @@ namespace Studentify.IntegrationTests.GetDto
         public int AuthorId { get; set; }
         public List<StudentifyAccount> Participants { get; set; }
         public int MaxNumberOfParticipants { get; set; }
+
+        private static bool CompareWithDto(MeetingGetDto m, MeetingDto dto)
+        {
+            if (m is null) return false;
+            if (dto is null) return false;
+            if (m.Name != dto.Name) return false;
+            if (m.ExpiryDate != dto.ExpiryDate) return false;
+            if (m.Description != dto.Description) return false;
+            if (m.Description != dto.Description) return false;
+            if (m.MaxNumberOfParticipants != dto.MaxNumberOfParticipants) return false;
+            return true;
+        }
+
+        private static bool Compare(MeetingGetDto m1, MeetingGetDto m2)
+        {
+            if (m1 is null) return false;
+            if (m2 is null) return false;
+            if (m1.Id != m2.Id) return false;
+            if (m1.EventType != m2.EventType) return false;
+            if (m1.Name != m2.Name) return false;
+            if (m1.CreationDate != m2.CreationDate) return false;
+            if (m1.ExpiryDate != m2.ExpiryDate) return false;
+            if (m1.Description != m2.Description) return false;
+            if (m1.AuthorId != m2.AuthorId) return false;
+            if (m1.MaxNumberOfParticipants != m2.MaxNumberOfParticipants) return false;
+
+            if (m1.Participants is null && m2.Participants is null) return true;
+            if (m1.Participants is null ^ m2.Participants is null) return false;
+            var firstNotSecond = m1.Participants.Except(m2.Participants).ToList();
+            var secondNotFirst = m2.Participants.Except(m1.Participants).ToList();
+            if (firstNotSecond.Any() || secondNotFirst.Any()) return false;
+            return true;
+        }
+
+        public static bool operator ==(MeetingGetDto a, MeetingDto dto)
+        {
+            return CompareWithDto(a, dto);
+        }
+
+        public static bool operator !=(MeetingGetDto a, MeetingDto dto)
+        {
+            return !CompareWithDto(a, dto);
+        }
+
+        public static bool operator ==(MeetingGetDto m1, MeetingGetDto m2)
+        {
+            return Compare(m1, m2);
+        }
+
+        public static bool operator !=(MeetingGetDto m1, MeetingGetDto m2)
+        {
+            return !Compare(m1, m2);
+        }
+
+        public override bool Equals(object o)
+        {
+            MeetingDto dto = o as MeetingDto;
+            if (dto is null) return false;
+            return CompareWithDto(this, dto);
+        }
     }
 }
--- Studentify.IntegrationTests/TestMeetings.cs
+++ Studentify.IntegrationTests/TestMeetings.cs
@@ -27,6 +27,14 @@ namespace Studentify.IntegrationTests
             LastName = "Testowsky",
             Password = "Test123!"
         };
+        private readonly RegisterDto _testAttendant = new()
+        {
+            Username = "Random_attendant",
+            Email = "random.attendant@test.test",
+            FirstName = "Random",
+            LastName = "Testowsky",
+            Password = "Test123!"
+        };
         private MeetingDto _testMeeting = new()
         {
             Name = "TestMeeting",
@@ -44,23 +52,29 @@ namespace Studentify.IntegrationTests
             },
             MaxNumberOfParticipants = 1
         };
+        private MeetingGetDto _testMeetingGetGto;
 
         [OneTimeSetUp]
         public async Task PrepareAccountAndClient()
         {
             _client = _factory.CreateClient();
-            await Register();
-            _loginData = await Login();
+            await Register(_testUser);
+            await Register(_testAttendant);
+            _loginData = await Login(new LoginDto
+            {
+                Username = _testUser.Username,
+                Password = _testUser.Password
+            });
         }
 
-        public async Task Register()
+        public async Task Register(RegisterDto user)
         {
             using var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
                 RequestUri = new Uri("api/authenticate/register", UriKind.Relative),
                 Content = new StringContent(
-                    JsonSerializer.Serialize(_testUser),
+                    JsonSerializer.Serialize(user),
                     Encoding.UTF8,
                     MediaTypeNames.Application.Json)
             };
@@ -69,19 +83,14 @@ namespace Studentify.IntegrationTests
             response.EnsureSuccessStatusCode();
         }
 
-        private async Task<LoginGetDto> Login()
+        private async Task<LoginGetDto> Login(LoginDto user)
         {
             using var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Post,
                 RequestUri = new Uri("api/authenticate/login", UriKind.Relative),
                 Content = new StringContent(
-                    JsonSerializer.Serialize(
-                    new LoginDto
-                    {
-                        Username = _testUser.Username,
-                        Password = _testUser.Password
-                    }),
+                    JsonSerializer.Serialize(user),
                     Encoding.UTF8,
                     MediaTypeNames.Application.Json)
             };
@@ -92,8 +101,8 @@ namespace Studentify.IntegrationTests
             return JsonSerializer.Deserialize<LoginGetDto>(
                 body, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
-
-        private async Task<HttpResponseMessage> SendAuthorizedRequest(HttpMethod method, String uri, MeetingDto dto)
+       
+        private async Task<HttpResponseMessage> SendAuthorizedRequest<DtoType>(HttpMethod method, string uri, DtoType dto)
         {
             using var request = new HttpRequestMessage
             {
@@ -107,35 +116,159 @@ namespace Studentify.IntegrationTests
             return await _client.SendAsync(request);
         }
 
-        [Test]
-        public async Task TestGetMeetings()
+        private async Task<HttpResponseMessage> SendAuthorisedNoBodyRequest(HttpMethod method, string uri)
         {
-            Assert.Pass();
+            using var request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = new Uri(uri, UriKind.Relative)
+            };
+            request.Headers.Add("Authorization", "Bearer " + _loginData.Token);
+            return await _client.SendAsync(request);
+        }
+
+        private async Task<MeetingGetDto> Deserialize(HttpResponseMessage response)
+        {
+            var body = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<MeetingGetDto>(
+                body, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        private async Task<List<MeetingGetDto>> DeserializeList(HttpResponseMessage response)
+        {
+            var body = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<List<MeetingGetDto>>(
+                body, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
         [Test]
+        [Order(1)]
         public async Task TestPostMeeting()
         {
             var response = await SendAuthorizedRequest(HttpMethod.Post, "/api/Meetings", _testMeeting);
             response.EnsureSuccessStatusCode();
+            _testMeetingGetGto = await Deserialize(response);
         }
 
         [Test]
+        [Order(2)]
+        public async Task TestGetMeetings()
+        {
+            var response = await SendAuthorisedNoBodyRequest(HttpMethod.Get, "/api/Meetings/");
+            response.EnsureSuccessStatusCode();
+            var meetings = await DeserializeList(response);
+            var meeting = (from m in meetings where m == _testMeetingGetGto select m).FirstOrDefault();
+            Assert.IsNotNull(meeting);
+        }
+
+        [Test]
+        [Order(2)]
         public async Task TestGetMeetingById()
         {
-            Assert.Pass();
+            var uri = string.Format("/api/Meetings/{0}", _testMeetingGetGto.Id);
+            var response = await SendAuthorisedNoBodyRequest(HttpMethod.Get, uri);
+            response.EnsureSuccessStatusCode();
+            var receivedMeeting = await Deserialize(response);
+            Assert.True(_testMeetingGetGto == receivedMeeting);
         }
 
         [Test]
+        [Order(4)]
         public async Task TestPatchMeetingById()
         {
-            Assert.Pass();
+            _testMeeting.Name = "patch_" + _testMeeting.Name;
+            _testMeeting.ExpiryDate = _testMeeting.ExpiryDate.AddDays(2);
+            _testMeeting.Description = "Test patching event.";
+            _testMeeting.Longitude = 0;
+            _testMeeting.Latitude += 1.5;
+            _testMeeting.Address = new()
+            {
+                Country = "Niemcy",
+                Town = "Berlin",
+                PostalCode = "00-000",
+                Street = "Geschlechtsverkehrstrasse",
+                HouseNumber = "18"
+            };
+            _testMeeting.MaxNumberOfParticipants += 1;
+
+            var uri = string.Format("/api/Meetings/{0}", _testMeetingGetGto.Id);
+            var response = await SendAuthorizedRequest(HttpMethod.Patch, uri, _testMeeting);
+            response.EnsureSuccessStatusCode();
+
+            var getUri = string.Format("/api/Meetings/{0}", _testMeetingGetGto.Id);
+            var getResponse = await SendAuthorisedNoBodyRequest(HttpMethod.Get, getUri);
+            response.EnsureSuccessStatusCode();
+            var receivedMeeting = await Deserialize(getResponse);
+            Assert.True(receivedMeeting == _testMeeting);
         }
 
         [Test]
-        public async Task TestPatchAttend()
+        [Order(3)]
+        public async Task FailToAddOwnerAsAttendant()
         {
-            Assert.Pass();
+            var uri = string.Format("/api/Meetings/attend/{0}", _testMeetingGetGto.Id);
+            var response = await SendAuthorisedNoBodyRequest(HttpMethod.Patch, uri);
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                Assert.Fail();
+            }
+            catch (HttpRequestException)
+            {
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        [Order(3)]
+        public async Task TestPatchAttendToMeeting()
+        {
+            var ownerClient = _client;
+            var ownerLoginData = _loginData;
+
+            _client = _factory.CreateClient();
+            _loginData = await Login(new LoginDto
+            {
+                Username = _testAttendant.Username,
+                Password = _testAttendant.Password
+            });
+
+            var uri = string.Format("/api/Meetings/attend/{0}", _testMeetingGetGto.Id);
+            var response = await SendAuthorisedNoBodyRequest(HttpMethod.Patch, uri);
+            response.EnsureSuccessStatusCode();
+
+            _client = ownerClient;
+            _loginData = ownerLoginData;
+        }
+
+        [Test]
+        [Order(4)]
+        public async Task FailToAddTheSameAttendantTwice()
+        {
+            var ownerClient = _client;
+            var ownerLoginData = _loginData;
+
+            _client = _factory.CreateClient();
+            _loginData = await Login(new LoginDto
+            {
+                Username = _testAttendant.Username,
+                Password = _testAttendant.Password
+            });
+
+            var uri = string.Format("/api/Meetings/attend/{0}", _testMeetingGetGto.Id);
+            var response = await SendAuthorisedNoBodyRequest(HttpMethod.Patch, uri);
+            try
+            {
+                response.EnsureSuccessStatusCode();
+                Assert.Fail();
+            }
+            catch (HttpRequestException)
+            {
+                Assert.Pass();
+            }
+            
+            _client = ownerClient;
+            _loginData = ownerLoginData;
         }
     }
 }
