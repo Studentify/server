11948852210b952621e56482a9ff29f6f0f671ec
Fillipek <marcin.filipek.321@gmail.com>
2021-06-01 19:19:05 +0200

    Add MeetingGetDto

---
+++ Studentify.IntegrationTests/GetDto/MeetingGetDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Studentify.Models.HttpBody;
+using Studentify.Models;
+
+namespace Studentify.IntegrationTests.GetDto
+{
+    class MeetingGetDto
+    {
+        public int Id { get; set; }
+        public String EventType { get; set; }
+        public String Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public String Description { get; set; }
+        public int AuthorId { get; set; }
+        public List<StudentifyAccount> Participants { get; set; }
+        public int MaxNumberOfParticipants { get; set; }
+    }
+}
